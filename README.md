2ca290fdb3b266d00aa3faba2798376a7c5c33ef
Shabab Salehin <salehin4axiz@gmail.com>
2023-09-06 00:00:40 +0600

    initial commit

---
+++ README.md
@@ -0,0 +1,1 @@
+This is a Cart Web Application built by Shabab Salehin. The technology used for this Web Application is ReactJs.
---
+++ src/data.js
@@ -0,0 +1,111 @@
+export const storeProducts = [
+  {
+    id: 1,
+    title: "Google Pixel - Black",
+    img: "img/product-1.png",
+    price: 40000,
+    company: "GOOGLE",
+    info:
+      "Unlock the world in stunning black and white with Google Pixel-Black: Where monochrome meets modernity, redefining smartphone sophistication. Experience the elegance of Google Pixel-Black: A sleek, monochromatic masterpiece that blends style and performance seamlessly. With its cutting-edge features and timeless design, it's the black mobile you've been waiting for.",
+    inCart: false,
+    count: 0,
+    total: 0
+  },
+  {
+    id: 2,
+    title: "Samsung S22 Ultra",
+    img: "img/product-2.png",
+    price: 70000,
+    company: "SAMSUNG",
+    info:
+      "Discover the magic of Samsung S22 Ultra, where innovation meets elegance. This strikingly pure and pristine smartphone redefines sophistication with its all-white design that exudes class and refinement. Step into a world of endless possibilities with Pixel-White's cutting-edge features, from its powerful camera to its lightning-fast performance. It's not just a phone; it's a statement of style and substance.With Samsung S22 Ultra, you'll experience the ultimate blend of aesthetics and technology, making it the perfect companion for those who demand the best. Elevate your mobile game and showcase your unique taste with this iconic, all-white masterpiece.",
+    inCart: false,
+    count: 0,
+    total: 0
+  },
+  {
+    id: 3,
+    title: "HTC U23 - Black",
+    img: "img/product-3.png",
+    price: 80000,
+    company: "htc",
+    info:
+      "Experience the future of smartphones with HTC U23-Black: Where sleek design meets unmatched performance. Get ready to immerse yourself in a world of sophistication and power with this stunning all-black device. HTC U23-Black is not just a phone; it's a statement of style and innovation. Its elegant black exterior conceals a powerhouse of features, from its exceptional camera capabilities to its lightning-fast processing speed.",
+    inCart: false,
+    count: 0,
+    total: 0
+  },
+  {
+    id: 4,
+    title: "HTC U23 - White",
+    img: "img/product-4.png",
+    price: 75000,
+    company: "htc",
+    info:
+      "Embrace timeless elegance with HTC U23-White: Where purity meets performance. This pristine all-white smartphone is more than just a device; it's a symbol of style and innovation. HTC U23-White combines a stunning design with powerful features, from its exceptional camera to its lightning-fast performance. It's the perfect companion for those who demand excellence in both aesthetics and technology.",
+    inCart: false,
+    count: 0,
+    total: 0
+  },
+  {
+    id: 5,
+    title: "HTC Desire 20+",
+    img: "img/product-5.png",
+    price: 85000,
+    company: "htc",
+    info:
+      "HTC Desire 20+: Where power meets style. This sleek smartphone offers exceptional performance and a stunning design. With a powerful camera, fast processing, and a sleek look, it's your perfect companion. Experience the future of mobile technology with HTC Desire 20+.",
+    inCart: false,
+    count: 0,
+    total: 0
+  },
+  {
+    id: 6,
+    title: "Vintage Iphone",
+    img: "img/product-6.png",
+    price: 20000,
+    company: "apple",
+    info:
+      "Rediscover the classics with the Vintage iPhone: A timeless icon that blends nostalgia with modern convenience. Experience the charm of yesteryears in a sleek, retro design that pays homage to the past while offering today's connectivity. Elevate your style with a Vintage iPhone, where old meets new in perfect harmony.",
+    inCart: false,
+    count: 0,
+    total: 0
+  },
+  {
+    id: 7,
+    title: "Iphone 13 Pro Max",
+    img: "img/product-7.png",
+    price: 150000,
+    company: "apple",
+    info:
+      "Experience the pinnacle of iPhone innovation with the iPhone 13 Pro Max: Where cutting-edge technology meets maximum performance. From its stunning ProMotion display to its powerful camera system, it's the ultimate device for those who demand the best. Elevate your mobile experience with the iPhone 13 Pro Max.",
+    inCart: false,
+    count: 0,
+    total: 0
+  },
+  {
+    id: 8,
+    title: "Smashed Iphone",
+    img: "img/product-8.png",
+    price: 5000,
+    company: "apple",
+    info:
+      "Discover the unbeatable deal of the century: Fully functional Smashed iPhones! These incredible devices might have a few cracks, but their performance is flawless. Get the best of both worlds with a working iPhone at a fraction of the cost. Don't let a few bumps and bruises deter you – embrace the savings without compromising on quality. Get your hands on a perfectly working, albeit slightly smashed, iPhone today and redefine what's possible!",
+    inCart: false,
+    count: 0,
+    total: 0
+  }
+];
+
+export const detailProduct = {
+  id: 1,
+  title: "Google Pixel - Black",
+  img: "img/product-1.png",
+  price: 40000,
+  company: "google",
+  info:
+    "Unlock the world in stunning black and white with Google Pixel-Black: Where monochrome meets modernity, redefining smartphone sophistication. Experience the elegance of Google Pixel-Black: A sleek, monochromatic masterpiece that blends style and performance seamlessly. With its cutting-edge features and timeless design, it's the black mobile you've been waiting for.",
+  inCart: false,
+  count: 0,
+  total: 0
+};
