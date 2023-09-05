7b9ac48c4ee02ce4a788e6d1d103c0dc82749901
Shabab Salehin <salehin4axiz@gmail.com>
2023-09-06 00:06:57 +0600

    Updates

---
+++ precache-manifest.e20f55edd3b623b367a8643ae949ec94.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "60006b92f263c428840f1a480ca2f9b8",
+    "url": "/salehin-cart/index.html"
+  },
+  {
+    "revision": "3a4b408371ac1eee5fe5",
+    "url": "/salehin-cart/static/css/2.0624914a.chunk.css"
+  },
+  {
+    "revision": "6bc876b157502d6a660a",
+    "url": "/salehin-cart/static/css/main.e811489f.chunk.css"
+  },
+  {
+    "revision": "3a4b408371ac1eee5fe5",
+    "url": "/salehin-cart/static/js/2.12229f6c.chunk.js"
+  },
+  {
+    "revision": "6bc876b157502d6a660a",
+    "url": "/salehin-cart/static/js/main.6060df18.chunk.js"
+  },
+  {
+    "revision": "595be9e0614983a14115",
+    "url": "/salehin-cart/static/js/runtime~main.ea4036e0.js"
+  },
+  {
+    "revision": "3285c592c0983be74e10458d74017da7",
+    "url": "/salehin-cart/static/media/logo.3285c592.png"
+  }
+]);
